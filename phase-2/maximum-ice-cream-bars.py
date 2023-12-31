a962fb2f344121c1f7ee5cdfc7b26d0c8b68a71a
EuL <101369119+ETjoel@users.noreply.github.com>
2023-12-31 10:46:04 +0300

    Added solution for Maximum Ice Cream Bars

---
+++ phase-2/maximum-ice-cream-bars.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        for i in range(len(costs)):
+            if coins - costs[i] < 0: return i
+            coins -= costs[i]
+        return len(costs)
