5e0856f2d862f6b35226dd5add5b67e1a83ab89e
EuL <101369119+ETjoel@users.noreply.github.com>
2024-02-08 15:41:19 +0300

    Add solution for Count Complete Subarrays in an Array

--- phase-2/leetcode/count-complete-subarrays-in-an-array.py
+++ phase-2/leetcode/count-complete-subarrays-in-an-array.py
@@ -1,12 +1,13 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        all_nums = set(nums)
-        res = 0
+        all_nums = len(set(nums))
+        wind = {}
+        res = j = 0
         for i in range(len(nums)):
-            wind = set()
-            for j in range(i, len(nums)):
-                wind.add(nums[j])
-                if wind == all_nums:
-                    res += len(nums) - j
-                    break
+            wind[nums[i]] = wind.get(nums[i], 0) + 1
+            while len(wind) == all_nums:
+                res += len(nums) - i
+                wind[nums[j]] -= 1
+                if wind[nums[j]] < 1: del wind[nums[j]]
+                j +=1
         return res
