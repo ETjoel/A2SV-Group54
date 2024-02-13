21212191e0a0fabbd69597bcdc58c3dcdd509add
EuL <101369119+ETjoel@users.noreply.github.com>
2024-02-13 11:34:13 +0300

    Add solution for Odd Even Linked List

---
+++ phase-2/leetcode/odd-even-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        slow, fast, follow = head, head.next.next, head.next
+        odd = True
+        while fast:
+            if odd:
+                follow.next = fast.next
+                fast.next = slow.next
+                slow.next = fast
+                slow = slow.next
+                fast = follow.next
+                odd = False
+            else:
+                follow = fast
+                fast = fast.next
+                odd = True
+        return head
