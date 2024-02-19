1c23cf569d269a8f0136463715a2e58faf857322
EuL <101369119+ETjoel@users.noreply.github.com>
2024-02-19 11:32:51 +0300

    Add solution for Implement Stack using Queues

---
+++ phase-2/leetcode/implement-stack-using-queues.py
@@ -0,0 +1,25 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+       self.queue = [x] + self.queue
+
+    def pop(self) -> int:
+        top = self.queue[0]
+        self.queue = self.queue[1:]
+        return top
+    def top(self) -> int:
+        return  self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
